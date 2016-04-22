17951bb7de62d8baa4aefaa78009320ca2612718
Michele Renda <michele.renda@gmail.com>
2016-04-22 15:35:02 +0300

    Added support for cmake and moved directory plot

--- src/graph.c
+++ src/graph.c
@@ -39,23 +39,23 @@ void created_graphs(char* output_dir, char* input_dir, char* exe_path, double mi
 	
 	
 	// Plotting grow linear
-	sprintf(buffer, "ctioga2 --csv --load '%s/grow_summary.csv' -f '%s/util/plot/graph-grow-linear.ct2' --output-directory '%s' --name 'graph-grow-linear'", input_dir, exe_path, output_dir);
+	sprintf(buffer, "ctioga2 --csv --load '%s/grow_summary.csv' -f '%s/plot/graph-grow-linear.ct2' --output-directory '%s' --name 'graph-grow-linear'", input_dir, exe_path, output_dir);
 	run_ctioga2(buffer, "graph-grow-linear", debug_flag);
 	
 	// Plotting grow linear with fit
-	sprintf(buffer, "ctioga2 --csv --load '%s/grow_summary.csv' --set min_t %.10E --set max_t %.10E --set min_fit_volume %.2f --set max_fit_volume %.2f --set fit_n %.10f --set fit_k %.10E  --set theo_n %.10f --set theo_k %.10E -f '%s/util/plot/graph-grow-linear-fit.ct2' --output-directory '%s' --name 'graph-grow-linear-fit'", input_dir, min_t, max_t, min_fit_volume, max_fit_volume, fit_n, fit_k, theo_n, theo_k, exe_path, output_dir);
+	sprintf(buffer, "ctioga2 --csv --load '%s/grow_summary.csv' --set min_t %.10E --set max_t %.10E --set min_fit_volume %.2f --set max_fit_volume %.2f --set fit_n %.10f --set fit_k %.10E  --set theo_n %.10f --set theo_k %.10E -f '%s/plot/graph-grow-linear-fit.ct2' --output-directory '%s' --name 'graph-grow-linear-fit'", input_dir, min_t, max_t, min_fit_volume, max_fit_volume, fit_n, fit_k, theo_n, theo_k, exe_path, output_dir);
 	run_ctioga2(buffer, "graph-grow-linear-fit", debug_flag);
 	
 	// Plotting grow log
-	sprintf(buffer, "ctioga2 --csv --load '%s/grow_summary_log.csv' -f '%s/util/plot/graph-grow-log.ct2'    --output-directory '%s' --name 'graph-grow-log'",    input_dir, exe_path, output_dir);
+	sprintf(buffer, "ctioga2 --csv --load '%s/grow_summary_log.csv' -f '%s/plot/graph-grow-log.ct2'    --output-directory '%s' --name 'graph-grow-log'",    input_dir, exe_path, output_dir);
 	run_ctioga2(buffer, "graph-grow-log", debug_flag);
 	
 	// Plotting grow log with fit
-	sprintf(buffer, "ctioga2 --csv --load '%s/grow_summary_log.csv' --set min_ln_t %.10E --set max_ln_t %.10E --set min_fit_volume %.2f --set max_fit_volume %.2f --set fit_n %.10f --set fit_k %.10E --set theo_n %.10f --set theo_k %.10E -f '%s/util/plot/graph-grow-log-fit.ct2'    --output-directory '%s' --name 'graph-grow-log-fit'", input_dir, log(min_t), log(max_t), min_fit_volume, max_fit_volume, fit_n, fit_k, theo_n, theo_k, exe_path, output_dir);
+	sprintf(buffer, "ctioga2 --csv --load '%s/grow_summary_log.csv' --set min_ln_t %.10E --set max_ln_t %.10E --set min_fit_volume %.2f --set max_fit_volume %.2f --set fit_n %.10f --set fit_k %.10E --set theo_n %.10f --set theo_k %.10E -f '%s/plot/graph-grow-log-fit.ct2'    --output-directory '%s' --name 'graph-grow-log-fit'", input_dir, log(min_t), log(max_t), min_fit_volume, max_fit_volume, fit_n, fit_k, theo_n, theo_k, exe_path, output_dir);
 	run_ctioga2(buffer, "graph-grow-log-fit", debug_flag);
 	
 	//// Plotting histograms
-	//sprintf(buffer, "ctioga2 --csv --load '%s/clusters_details.csv' -f '%s/util/plot/hist-volume.ct2'    --output-directory '%s' --name 'hist-volume'", input_dir, exe_path, output_dir);
+	//sprintf(buffer, "ctioga2 --csv --load '%s/clusters_details.csv' -f '%s/plot/hist-volume.ct2'    --output-directory '%s' --name 'hist-volume'", input_dir, exe_path, output_dir);
 	//run_ctioga2(buffer, "hist-volume", debug_flag);
 }
 
